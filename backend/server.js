d62debc1c293b154307b89c167d93d69b0a006cb
guilhermecolares <guilhermegraf17@gmail.com>
2026-07-09 12:53:59 -0300

    Vercel + Render Connection

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ import authRoutes from './routes/auth.js'
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- frontend/src/stores/useAuthStore.js
+++ frontend/src/stores/useAuthStore.js
@@ -10,7 +10,7 @@ export const useAuthStore = create(
       userName: '',
 
       login: async (email, password) => {
-        const response = await fetch('http://localhost:5000/api/auth/login', {
+        const response = await fetch('https://motask-api.onrender.com/api/auth/login', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password })
@@ -30,7 +30,7 @@ export const useAuthStore = create(
       },
 
       register: async (name, email, password) => {
-        const response = await fetch('http://localhost:5000/api/auth/register', {
+        const response = await fetch('https://motask-api.onrender.com/api/auth/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ name, email, password })
